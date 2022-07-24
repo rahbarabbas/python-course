686aa63a16a077208608930efb3fd6c1aa06c204
rahbarabbas <rahbar1976@gmail.com>
2022-07-24 12:06:57 +0530

    Create t3.py

---
+++ t3.py
@@ -0,0 +1,18 @@
+from turtle import *
+color('VIOLET')
+forward(150) 
+left(144) 
+color('RED')   
+forward(150)
+left(144)
+color('BLUE')
+forward(150)
+left(144)
+color('GREEN')
+forward(150)
+left(144)
+color('ORANGE')
+forward(150)
+
+
+mainloop() # stops the window from closing
