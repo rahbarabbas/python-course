678a68a55c0712541a3c6931b636c613c70fcde5
rahbarabbas <rahbar1976@gmail.com>
2022-07-27 11:53:32 +0530

    Create t6.py

---
+++ t6.py
@@ -0,0 +1,12 @@
+from turtle import *
+speed('slowest')
+penup()
+goto(-200,200)
+goto(200,200)
+pendown()
+goto(200,-200)
+write('  I am here',font=('arial',30,'normal'),align='center')
+goto(-200,-200)
+write('I am here')
+
+mainloop()
