34d129b0bf631f42a8ad31f8b374ccab4b277d36
rahbarabbas <rahbar1976@gmail.com>
2022-07-31 11:48:08 +0530

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,15 @@
+# Accessing string characters in Python
+str='digipodium'
+print('str=',str)
+
+#first character
+print('str[0]=',str[0])
+
+#second character
+print('str[1]=',str[1])
+
+#last character
+print('str[-1]=',str[-1])
+
+#second last character
+print('str[-2]=',str[-2])
