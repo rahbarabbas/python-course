01d7e045a1b26a0661bbeca8d666f77ffbb5c4ae
rahbarabbas <rahbar1976@gmail.com>
2022-07-31 11:48:00 +0530

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,15 @@
+# defining strings in python
+# all of the following are equivalent
+
+my_string='Hello'
+print(my_string)
+
+my_string="Hello"
+print(my_string)
+
+my_string='''Hello'''
+print(my_string)
+
+# triple code strings can extend multiple lines
+my_string="""Hello,welcome to the world of Python"""
+print(my_string)
